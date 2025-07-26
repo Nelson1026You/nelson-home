9bb3868d35a9596bfd7bb3e9845618cb92a8bb67
Nelson You <q775063@gmail.com>
2025-07-26 11:30:51 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+// import React from 'react'
 function App() {
   return (
     <>
       <div>
         <h1>hello world, this is nelson, I am a Full-stack developer</h1>
+        <h1>This is my website</h1>
       </div>
     </>
   )
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
-  base: 'nelson.github.io', 
+  base: '/nelson-home/', 
 })
